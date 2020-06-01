344724610db0251d67badf2e6ed420c127d592dd
Centrix14 <artiom.zvyagin@yandex.ru>
2020-06-01 18:09:57 +0300

    minimalized

--- fll.c
+++ fll.c
@@ -4,16 +4,6 @@
 
 char buf[BUFSIZ] = "\0";
 
-void *fll_malloc_s(int size) {
-	char *ptr = malloc(size);
-
-	if (!ptr) {
-		perror(__func__);
-		exit(1);
-	}
-	return ptr;
-}
-
 FILE *fll_fopen_s(char *fname, char *mode) {
 	FILE *ptr = fopen(fname, mode);
 
@@ -24,41 +14,7 @@ FILE *fll_fopen_s(char *fname, char *mode) {
 	return ptr;
 }
 
-void fll_free_s(char *ptr) {
-	if (ptr) {
-		free(ptr);
-		//printf(" ");	
-	}
-}
-
-int fll_get_file_size(char *fname) {
-	struct stat buf;
-
-	stat(fname, &buf);
-	return buf.st_size;
-}
-
 char *fll_read_from_file(char *fname) {
-	int size = fll_get_file_size(fname), c = 0, i = 0;
-	static char *buffer = NULL;
-	FILE *src = NULL;
-
-	buffer = fll_malloc_s(sizeof(char) * size);
-
-	src = fll_fopen_s(fname, "r");
-	c = fgetc(src);
-	while (!feof(src)) {
-		buffer[i++] = c;	
-		c = fgetc(src);
-	}
-	buffer[i] = '\0';
-
-	fclose(src);
-
-	return buffer;
-}
-
-char *fll_read_from_file_static(char *fname) {
 	int c = 0, i = 0;
 	FILE *src = NULL;
 
